10446d80d61647a38af99e854014462b7c4eb8e5
rafaelrnzo <rl.lorenzo.256@gmail.com>
2024-01-22 15:01:59 +0700

    update layout admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,9 @@
 @section('content')
     <section class="bg-gray-50 dark:bg-gray-900 pt-24 px-4 md:px-12 lg:px-44 flex flex-col gap-4 ">
         <span class="text-2xl text-white">
-            Hello, Welcome back {{Auth::user()->name}}!
+            @if (Auth::user())
+                Hello, Welcome back {{ Auth::user()->name }}!
+            @endif
         </span>
         <div class="max-w-screen">
             <div class="grid md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -10,13 +12,13 @@
                     <div
                         class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">
-                            <img class="rounded-t-lg h-44 w-full object-cover"
-                                src="{{ $product->thumbnail }} "
-                                alt="product image"  />
+                            <img class="rounded-t-lg h-44 w-full object-cover" src="{{ $product->thumbnail }} "
+                                alt="product image" />
                         </a>
                         <div class="px-5 py-5 flex flex-col gap-4 justify-between ">
                             <a href="#">
-                                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{$product->name}}</h5>
+                                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                    {{ $product->name }}</h5>
                             </a>
                             <div class="flex items-center justify-between w-full ">
                                 <span class="text-2xl font-bold text-gray-900 dark:text-white">{{ $product->price }}k</span>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -73,7 +73,7 @@
             </div>
         </nav>
         <div class="h-screen">
-
+            
             @yield('content')
         </div>
     </div>
